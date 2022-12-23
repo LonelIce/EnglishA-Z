7c4896369d48c9c78610139bce6f32039626e235
Lonel Ice <ijumrz@gmail.com>
2022-12-23 15:55:30 +0300

    Fix routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import './styles/globale.scss';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './pages/Login';
+import RegistrationForm from './components/RegistrationForm';
 
 const App = function () {
     return (
@@ -12,7 +13,11 @@ const App = function () {
             <Navbar />
             <Routes>
                 <Route path='/' element={<StartPage />} />
-                <Route path='/login/*' element={<Login />} />
+                <Route path='/login'>
+                    <Route index element={<Login />} />
+                    <Route path='registration' element={<RegistrationForm />} />
+                </Route>
+                <Route path='*' element={<>Not Found</>} />
             </Routes>
             <Footer />
         </>
--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,26 +1,16 @@
 import React, { FC } from 'react';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import styles from './Login.module.scss';
-import LoginForm from '../../components/LoginForm';
-import RegistrationForm from '../../components/RegistrationForm';
 
 const Login: FC = function () {
     return (
         <main className={styles.main}>
-            <Routes>
-                <Route
-                    path='/'
-                    element={
-                        <NavLink to='/login/registration'>
-                            <section>
-                                <h2>Зарегистрироваться</h2>
-                            </section>
-                        </NavLink>
-                    }
-                />
-                <Route path='/registration' element={<RegistrationForm />} />
-                <Route path='/authorization' element={<LoginForm />} />
-            </Routes>
+            <Link to='/login/registration'>
+                <section>
+                    <h2>Зарегистрироваться</h2>
+                </section>
+            </Link>
+            <Outlet />
         </main>
     );
 };
