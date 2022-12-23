39e05944281b47e66940189f02dd6ffb8be2611d
Lonel Ice <ijumrz@gmail.com>
2022-12-23 17:54:15 +0300

    Create simle login page

--- client/src/components/Navbar/utils/UserMenuLink/index.tsx
+++ client/src/components/Navbar/utils/UserMenuLink/index.tsx
@@ -6,7 +6,12 @@ const UserMenuLink: FC = function () {
     return (
         <li className={styles.userMenuLink}>
             <div className={styles.linkWrapper}>
-                <NavLink to='/login'>
+                <NavLink
+                    to='/login'
+                    className={({ isActive }) =>
+                        isActive ? styles.activeLink : ''
+                    }
+                >
                     <span className={styles.text}>Войти</span>
                 </NavLink>
             </div>
--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import styles from './Login.module.scss';
+import LoginForm from '../../components/LoginForm';
 
 const Login: FC = function () {
     return (
         <main className={styles.main}>
+            <LoginForm />
             <Link to='/login/registration'>
                 <section>
-                    <h2>Зарегистрироваться</h2>
+                    <h4>Зарегистрироваться</h4>
                 </section>
             </Link>
             <Outlet />
